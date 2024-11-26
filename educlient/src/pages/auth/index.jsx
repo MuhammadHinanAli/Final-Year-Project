f196b712ca42044939da58b6b5411a4d7c87db55
Muhammad Hinan Ali <G00409830@atu.ie>
2024-11-26 19:21:59 +0000

    Sign up page some work done (form set / shadcn used)

--- educlient/src/pages/auth/index.jsx
+++ educlient/src/pages/auth/index.jsx
@@ -1,4 +1,6 @@
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import CommonForm from "@/components/common-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {signUpFormControls } from "@/config";
 import { GraduationCap } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -18,17 +20,21 @@ function AuthPage() {
                     <span className="font-extrabold text-xl">EduQuest</span>
                 </Link>
             </header>
-            <div className="flex item-center justify-center min-h-screen bg-background">
+            <div className="flex items-center justify-center min-h-screen bg-background">
                 <Tabs
                     value={activeTab}
                     defaultValue="signin"
                     onValueChange={handleTabChange}
                     className="w-full max-w-md"
                 >
-                    <TabsList>
-                        <TabsTrigger>Sign In</TabsTrigger>
-                        <TabsTrigger>Sign Up</TabsTrigger>
+                    <TabsList className="grid w-full grid-cols-2">
+                        <TabsTrigger value="signin">Sign In</TabsTrigger>
+                        <TabsTrigger value="signup">Sign Up</TabsTrigger>
                     </TabsList>
+                    <TabsContent value="signin">signin</TabsContent>
+                    <TabsContent value="signup">
+                    <CommonForm formControls={signUpFormControls}/>
+                    </TabsContent>
                 </Tabs>
             </div>
         </div>

aa37667f8b56a527ac250c5bd2adc6da2cca5493
Muhammad Hinan Ali <G00409830@atu.ie>
2025-01-20 19:42:26 +0000

    cloudinary integrated in backend for deleting and uploading

--- eduserver/server.js
+++ eduserver/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth-routes/index");
+const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -25,6 +26,7 @@ mongoose
 
 //ROUTES CONFIGURATION
 app.use("/auth", authRoutes);
+app.use("/media", mediaRoutes);
 
 app.use((err, req, res, next) => {
     console.log(err.stack);

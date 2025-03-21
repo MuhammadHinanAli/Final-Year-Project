8874bc8878c9e125bf9492c0159eeb036147060e
Muhammad Hinan Ali <G00409830@atu.ie>
2025-03-21 19:21:00 +0000

    ..

--- educlient/src/pages/student/home/index.jsx
+++ educlient/src/pages/student/home/index.jsx
@@ -1,21 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { AuthContext } from "@/context/auth-context";
-import { useContext } from "react";
 
 function StudentHomePage() {
 
-    const {resetCredentials} = useContext(AuthContext)
 
-    function handleLogout() {
-        resetCredentials();
-        sessionStorage.clear();
-    }
-
-    return ( 
-        <div>Home Page
-            <Button onClick={handleLogout}>Logout</Button>
+    return (
+        <div className="min-h-screen bg-white ">
+            <section className="flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
+                <div className="lg:w-1/2 lg:pr-12 ">
+                    <h1 className="text-4xl font-bold mb-4">"Education is the most powerful weapon which you can use to change the world." ~ Nelson Mandela</h1>
+                    <p className="text-xl">Welcome to EduQuest, the ultimate online learning platform designed to empower students and educators. Whether you're looking to enhance your skills, access interactive courses, or manage educational resources efficiently, our LMS provides a seamless and engaging learning experience. With intuitive navigation, real-time progress tracking, and a vast library of courses, we make education accessible anytime, anywhere. Join us today and take the next step in your learning journey!</p>
+                </div>
+            </section>
         </div>
-     );
+    );
 }
 
 export default StudentHomePage;
